7508cf60b03cf6da1fcf9d94ee060641598221bf
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-05-18 21:05:37 -0700

    add parseHtml function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
+import Search from './components/Search';
+
 function App() {
   
 
   return (
     <>
-  
+      <Search />
     </>
   )
 }
--- client/src/components/search.jsx
+++ client/src/components/search.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import axios from 'axios';
 
 const Search = () => {
 
   const URL = "https://wa-whatcomcounty.civicplus.com/Archive.aspx?AMID=43";
 
-  const [searchTerm, setSearchTerm] = useState("");
-  const [searchResults, setSearchResults] = useState([]);
+  // const [searchTerm, setSearchTerm] = useState("");
+  // const [searchResults, setSearchResults] = useState([]);
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
+    console.log('Button clicked');
     try {
       const res = await axios.get(URL);
       const htmlContent = res.data;
@@ -18,13 +19,27 @@ const Search = () => {
     } catch (error) {
       console.log('Error parsing webpage:', error);
     }
+  }
 
-
+  const parseHtml = (htmlContent) => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(htmlContent, 'text/html');
+    const pdfLinks = Array.from(doc.querySelectorAll('a[href]')).reduce(
+      (links, link) => {
+        const href = link.getAttribute('href');
+        if (href.toLowerCase().endsWith('.pdf')) {
+          links.push(href);
+        }
+        return links;
+      },[]);
+    console.log(pdfLinks);
+    return pdfLinks;
+  };
   
-
 
     return (
         <>
+          <button onClick={handleFormSubmit}>Test Button</button> 
         </>
     )
 }
