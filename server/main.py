95b9d1bf9a3fdd69549c87cbdfe04aa30ecd1c3b
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-05-27 09:56:56 -0700

    add BeautfulSoup parsing to locate PDF links

--- server/main.py
+++ server/main.py
@@ -5,3 +5,13 @@ URL = "https://wa-whatcomcounty.civicplus.com/Archive.aspx?AMID=43";
 html_text = requests.get(URL).text
 
 soup = BeautifulSoup(html_text, 'html.parser')
+
+pdf_links = []
+
+for link in soup.find_all('a'):
+    links = link.get('href')
+    if links is not None:
+        link_list = links.split()
+        pdf_links.extend([pdf_link for pdf_link in link_list if pdf_link.startswith('Archive')])
+
+print(f'The PDF links are: {pdf_links}')
