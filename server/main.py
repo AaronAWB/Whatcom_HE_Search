7f5d57da5535f7b7c62c963a4c4071740886b7d5
mliu08 <liumar@oregonstate.edu>
2023-06-07 19:31:31 -0700

    minor format changes for flake8

--- server/main.py
+++ server/main.py
@@ -8,12 +8,13 @@ from io import BytesIO
 from pdfminer.high_level import extract_text
 from PIL import Image
 
-URL = "https://wa-whatcomcounty.civicplus.com/Archive.aspx?AMID=43";
+URL = "https://wa-whatcomcounty.civicplus.com/Archive.aspx?AMID=43"
 html = requests.get(URL).text
 
 soup = BeautifulSoup(html, 'html.parser')
 
 pdf_links = []
+
 
 def retrieve_pdf_links():
     for link in soup.find_all('a'):
@@ -21,13 +22,14 @@ def retrieve_pdf_links():
         if links is not None:
             link_list = links.split()
             pdf_links.extend([pdf_link for pdf_link in link_list if pdf_link.startswith('Archive.aspx?ADID')])
-    
+
+
 def search_keyword(pdf_links, keyword):
-    
+
     base_url = "https://wa-whatcomcounty.civicplus.com/"
     retrieve_pdf_links()
     print(pdf_links)
-    
+
     for link in pdf_links:
         complete_link = urllib.parse.urljoin(base_url, link)
         response = requests.get(complete_link)
@@ -42,10 +44,11 @@ def search_keyword(pdf_links, keyword):
         text = extract_text(BytesIO(pdf_content)).strip()
         if keyword in text:
             print(f"Keyword '{keyword}' found in PDF: {link}")
-                    
+
         if not text.strip():
             print(f"PDF is not text-based: {link}. Running OCR...")
             image_to_text(pdf_content, keyword, link)
+
 
 def get_metadata(pdf_content):
 
@@ -58,11 +61,13 @@ def get_metadata(pdf_content):
     except:
         print("No metadata found")
 
+
 def image_to_text(image_data, keyword, link):
     image = Image.open(BytesIO(image_data))
     extracted_text = pytesseract.image_to_string(image)
-    
+
     if keyword in extracted_text:
         print(f"Keyword '{keyword}' found in image from PDF: {link}")
+
 
 search_keyword(pdf_links, 'Whatcom')
