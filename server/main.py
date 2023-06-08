68d885e728b5d8205ba3fa3a43bb01e2635ef72b
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-06-08 15:02:23 -0700

    add comments

--- server/main.py
+++ server/main.py
@@ -16,10 +16,13 @@ soup = BeautifulSoup(html, 'html.parser')
 pdf_links = []
 
 def retrieve_pdf_links():
+    # Finds all <a> tags in the html and extracts the links
     for link in soup.find_all('a'):
         links = link.get('href')
         if links is not None:
             link_list = links.split()
+            
+            # Adds all links starting with 'Archive.aspx?ADID' to pdf_links
             pdf_links.extend([pdf_link for pdf_link in link_list if pdf_link.startswith('Archive.aspx?ADID')])
     
 def search_keyword(pdf_links, keyword):
@@ -34,15 +37,20 @@ def search_keyword(pdf_links, keyword):
         pdf_content = response.content
         get_metadata(pdf_content)
 
+        # Creates a PyPDF2 reader object to read the PDF content
         pdf_reader = PyPDF2.PdfReader(BytesIO(pdf_content))
+        
+        # Checks if the PDF has a valid /Root object
         if '/Root' not in pdf_reader.trailer:
             print(f"Invalid PDF: {link}")
             continue
-
+        
+        # Extracts the text from the PDF using pdfminer, deletes whitespace, and checks if the keyword is in the text
         text = extract_text(BytesIO(pdf_content)).strip()
         if keyword in text:
             print(f"Keyword '{keyword}' found in PDF: {link}")
-                    
+
+        # Checks if the PDF is text-based, if not, runs OCR on the PDF            
         if not text.strip():
             print(f"PDF is not text-based: {link}. Running OCR...")
             image_to_text(pdf_content, keyword, link)
@@ -50,6 +58,7 @@ def search_keyword(pdf_links, keyword):
 def get_metadata(pdf_content):
 
     try:
+        # Creates a PyPDF2 reader object to extract the metadata
         pdf_stream = BytesIO(pdf_content)
         pdf_reader = PyPDF2.PdfReader(pdf_stream)
         metadata = pdf_reader.metadata
@@ -59,7 +68,9 @@ def get_metadata(pdf_content):
         print("No metadata found")
 
 def image_to_text(image_data, keyword, link):
+    # Opens an image from the PDF using PIL
     image = Image.open(BytesIO(image_data))
+    # Pytesseract OCR extracts text fromt he image
     extracted_text = pytesseract.image_to_string(image)
     
     if keyword in extracted_text:
