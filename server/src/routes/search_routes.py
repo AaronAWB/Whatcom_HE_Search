74251f6c5b59d28c74a534d4a908d3afe833b28e
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-08-24 15:52:48 -0700

    refactor search route to search database instead of scraping

--- server/src/routes/search_routes.py
+++ server/src/routes/search_routes.py
@@ -2,11 +2,23 @@ from flask_restx import Resource
 
 from src import api
 from src.lib.WHE_scrape import whe_scrape
+from src.models.decisions import Decision
 
 @api.route('/search/<keyword>')
 class Search(Resource):
-  def get(self, keyword):
-    return {'search_results': whe_scrape.search_keyword(keyword)}
+    def get(self, keyword):
+        search_results = Decision.query.filter(or_(Decision.text.ilike(f'%{keyword}%')))
+        result_list = [{
+            'id': decision.id,
+            'case_name': decision.case_name,
+            'hearing_examiner': decision.hearing_examiner,
+            'hearing_date': decision.hearing_date,
+            'decision_date': decision.decision_date,
+            'text': decision.text,
+            'link': decision.link
+        } for decision in search_results]
+
+        return {'search_results': result_list}
 
 @api.route('/metadata')
 class Metadata(Resource):
