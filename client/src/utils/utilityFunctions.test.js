82954f231c4197527a6a036094aed6d517579a0e
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2023-08-31 15:19:03 -0700

    configure Vitest for frontend testing and add utilityFunctions.test file and tests

---
+++ client/src/utils/utilityFunctions.test.js
@@ -0,0 +1,14 @@
+import { expect, test } from 'vitest'
+import { formatDate } from '@Utils/utilityFunctions'
+
+test('formats a date from MM.DD.YYYY to Month DD, YYYY', () => {
+  const date = '8.25.2023'
+  const formattedDate = formatDate(date)
+  expect(formattedDate).toBe('August 25, 2023')
+})
+
+test('returns the date if it is not in the correct format', () => {
+  const date = 'August 25, 2023'
+  const formattedDate = formatDate(date)
+  expect(formattedDate).toBe(date)
+})
